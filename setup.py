ce98130c100ae264f30cbc54e5ef333ce7ace0ec
krypticmouse <herumbshandilya123@gmail.com>
2021-01-24 00:32:22 +0530

    Updated setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="CLTviz",
+    name="LiveStats",
     version="0.0.1",
     author="Herumb Shandilya",
     author_email="herumbshandilya123@gmail.com",
     description="Your Statistics Playground",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/krypticmouse/CLTviz",
+    url="",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
